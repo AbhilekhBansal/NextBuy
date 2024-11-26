2c7c2e2fe9cdaabedf9726a7e35df039029233e5
AbhilekhBansal <abhilekhbansal10@gmail.com>
2024-11-26 19:36:19 +0530

    update:images imports changes

--- src/components/brands.jsx
+++ src/components/brands.jsx
@@ -3,15 +3,15 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
 import brand1 from "@/assets/brands/brand1.png";
-import brand2 from "@/assets/brands/brand1.png";
-import brand3 from "@/assets/brands/brand1.png";
-import brand4 from "@/assets/brands/brand1.png";
-import brand5 from "@/assets/brands/brand1.png";
-import brand6 from "@/assets/brands/brand1.png";
-import brand7 from "@/assets/brands/brand1.png";
-import brand8 from "@/assets/brands/brand1.png";
-import brand9 from "@/assets/brands/brand1.png";
-import brand10 from "@/assets/brands/brand1.png";
+import brand2 from "@/assets/brands/brand2.png";
+import brand3 from "@/assets/brands/brand3.png";
+import brand4 from "@/assets/brands/brand4.png";
+import brand5 from "@/assets/brands/brand5.png";
+import brand6 from "@/assets/brands/brand6.png";
+import brand7 from "@/assets/brands/brand7.png";
+import brand8 from "@/assets/brands/brand8.png";
+import brand9 from "@/assets/brands/brand9.png";
+import brand10 from "@/assets/brands/brand10.png";
 
 const Brands = () => {
   return (
